72b41985ab4b6727f145d513977b445bc91842a8
bibibibi12 <dangkimthuy1234@gmail.com>
2021-09-29 19:49:53 +0700

    first commit

---
+++ Demo.Web/Pages/Counter.razor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Web.Pages
+{
+    public partial class Counter
+    {
+
+    private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+
+}
+}
---
+++ Demo.Web/Pages/FetchDataBase.cs
@@ -0,0 +1,21 @@
+﻿using Demo.Web.Data;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Web.Pages
+{
+    public class FetchDataBase : ComponentBase
+    {
+        public WeatherForecast[] forecasts;
+        public readonly WeatherForecastService _weatherForecastService = new WeatherForecastService();
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await _weatherForecastService.GetForecastAsync(DateTime.Now);
+        }
+    }
+        
+}
